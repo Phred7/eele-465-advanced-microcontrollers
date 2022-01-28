085857d1678356bf289e5b8328dcd21d30089f1b
W. Ward <woward00@gmail.com>
2022-01-28 10:46:41 -0700

    Project 2 ports working
    Make sure to remove the light sensor jumpers for P3.2 and P3.3

--- Adv_Microcontrollers_Project_2_ASM_Ward/Project2.asm
+++ Adv_Microcontrollers_Project_2_ASM_Ward/Project2.asm
@@ -23,8 +23,76 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
+init:
+				bis.b	#BIT6, &P6DIR ; set green LED2 as out
+				bic.b	#BIT6, &P6OUT ; set init val to 0
 
+				bis.b	#BIT0, &P1DIR ; set red LED1 as out
+				bic.b	#BIT0, &P1OUT ; set init val to 0
+
+				bis.b	#BIT2, &P3DIR			; set P3.2 as out for SCL
+				bic.b	#BIT2, &P3OUT			; set init val to 0
+
+				bis.b	#BIT3, &P3DIR			; set P3.3 as out for SDA
+				bic.b	#BIT3, &P3OUT			; set init val to 0
+
+				; setup timer TB0:
+				bis.w	#TBCLR, &TB0CTL 		; clears timers and dividers
+				bis.w	#TBSSEL__SMCLK, &TB0CTL	; choose clock (f = 1 MHz)
+				bis.w	#MC__UP, &TB0CTL		; choose mode (UP)
+
+				bis.w	#CNTL_0, &TB0CTL		; choose counter length (N = 2^16)
+				bis.w	#ID__4, &TB0CTL			; choose divider for D1 (D1 = 4)
+				bis.w 	#TBIDEX__8, &TB0EX0		; choose divider for D2 (D2 = 8)
+
+				; TB0 interrupt: Compare
+				mov.w	#32992d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for ~0.5Hz
+				bis.w	#CCIE, &TB0CCTL0
+				bic.w	#CCIFG, &TB0CCTL0
+
+				nop
+				eint							; assert global interrupt flag
+				nop
+
+				bic.b	#LOCKLPM5, &PM5CTL0		; disable DIO low-power default
+
+				bis.b	#BIT3, &P3OUT			; set init val to 0
+
+main:
+				xor.b	#BIT2, &P3OUT
+				xor.b	#BIT3, &P3OUT
+				mov.w	global_outer_delay, R4
+				call 	#delay
+				jmp main
+
+delay:
+				mov.w	global_inner_delay, R5	; sets inner loop delay
+dec_inner:		dec		R5						; decrements inner delay reg
+				jnz		dec_inner				; loop if R5 is not 0
+				dec		R4						; decrements outer delay reg
+				jnz		delay					; loop if R4 is not 0
+				ret
+
+;-------------------------------------------------------------------------------
+; Interrupt Service Routines
+;-------------------------------------------------------------------------------
+
+; Service TB0
+TimerB0_ISR:
+			xor.b	#BIT6, &P6OUT
+			bic.w	#TBIFG, &TB0CTL
+			reti
+;-------------- END service_TB0 --------------
                                             
+;-------------------------------------------------------------------------------
+; Memory Allocation
+;-------------------------------------------------------------------------------
+
+						.data
+						.retain
+
+global_outer_delay:		.short	000FFh
+global_inner_delay:		.short  0000Fh
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
@@ -38,3 +106,5 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
             .sect   ".reset"                ; MSP430 RESET Vector
             .short  RESET
             
+            .sect	".int43"				; TB0CCR0
+            .short	TimerB0_ISR
