89360e3a303c0fd900a4db4c71eeb84aac1866ca
W. Ward <woward00@gmail.com>
2022-01-31 01:05:56 -0700

    Project 2 tx_byte with addr done
    Needs testing

--- EELE465.02_I2C_ASM_Ward_Ketteler/Project2.asm
+++ EELE465.02_I2C_ASM_Ward_Ketteler/Project2.asm
@@ -3,83 +3,134 @@
 ;
 ;
 ;-------------------------------------------------------------------------------
-            .cdecls C,LIST,"msp430.h"       ; Include device header file
+            		.cdecls C,LIST,"msp430.h"       ; Include device header file
             
 ;-------------------------------------------------------------------------------
-            .def    RESET                   ; Export program entry-point to
-                                            ; make it known to linker.
+            		.def    RESET                   ; Export program entry-point to
+                                            		; make it known to linker.
 ;-------------------------------------------------------------------------------
-            .text                           ; Assemble into program memory.
-            .retain                         ; Override ELF conditional linking
-                                            ; and retain current section.
-            .retainrefs                     ; And retain any sections that have
-                                            ; references to current section.
+            		.text                           ; Assemble into program memory.
+            		.retain                         ; Override ELF conditional linking
+                                            		; and retain current section.
+            		.retainrefs                     ; And retain any sections that have
+                                            		; references to current section.
 
 ;-------------------------------------------------------------------------------
-RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
-StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
+RESET       		mov.w   #__STACK_END,SP         ; Initialize stackpointer
+StopWDT     		mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
 init:
-				bis.b	#BIT6, &P6DIR ; set green LED2 as out
-				bic.b	#BIT6, &P6OUT ; set init val to 0
+					bis.b	#BIT6, &P6DIR 			; set green LED2 as out - reps SCL
+					bic.b	#BIT6, &P6OUT 			; set init val to 0
 
-				bis.b	#BIT0, &P1DIR ; set red LED1 as out
-				bic.b	#BIT0, &P1OUT ; set init val to 0
+					bis.b	#BIT0, &P1DIR 			; set red LED1 as out - reps SDA
+					bic.b	#BIT0, &P1OUT 			; set init val to 0
 
-				bis.b	#BIT2, &P3DIR			; set P3.2 as out for SCL
-				bic.b	#BIT2, &P3OUT			; set init val to 0
+					bis.b	#BIT2, &P3DIR			; set P3.2 as out for SCL
+					bic.b	#BIT2, &P3OUT			; set init val to 0
 
-				bis.b	#BIT3, &P3DIR			; set P3.3 as out for SDA
-				bic.b	#BIT3, &P3OUT			; set init val to 0
+					bis.b	#BIT3, &P3DIR			; set P3.3 as out for SDA
+					bic.b	#BIT3, &P3OUT			; set init val to 0
 
-				; setup timer TB0:
-				bis.w	#TBCLR, &TB0CTL 		; clears timers and dividers
-				bis.w	#TBSSEL__SMCLK, &TB0CTL	; choose clock (f = 1 MHz)
-				bis.w	#MC__UP, &TB0CTL		; choose mode (UP)
+					; setup timer TB0:
+					bis.w	#TBCLR, &TB0CTL 		; clears timers and dividers
+					bis.w	#TBSSEL__SMCLK, &TB0CTL	; choose clock (f = 1 MHz)
+					bis.w	#MC__UP, &TB0CTL		; choose mode (UP)
 
-				bis.w	#CNTL_0, &TB0CTL		; choose counter length (N = 2^16)
-				bis.w	#ID__4, &TB0CTL			; choose divider for D1 (D1 = 4)
-				bis.w 	#TBIDEX__8, &TB0EX0		; choose divider for D2 (D2 = 8)
+					bis.w	#CNTL_0, &TB0CTL		; choose counter length (N = 2^16)
+					bis.w	#ID__4, &TB0CTL			; choose divider for D1 (D1 = 4)
+					bis.w 	#TBIDEX__8, &TB0EX0		; choose divider for D2 (D2 = 8)
 
-				; TB0 interrupt: Compare
-				mov.w	#32992d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for ~0.5Hz
-				bis.w	#CCIE, &TB0CCTL0
-				bic.w	#CCIFG, &TB0CCTL0
+					; TB0 interrupt: Compare
+					mov.w	#32992d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for ~0.5Hz
+					bis.w	#CCIE, &TB0CCTL0
+					bic.w	#CCIFG, &TB0CCTL0
 
-				nop
-				eint							; assert global interrupt flag
-				nop
+					nop
+					eint							; assert global interrupt flag
+					nop
 
-				bic.b	#LOCKLPM5, &PM5CTL0		; disable DIO low-power default
+					bic.b	#LOCKLPM5, &PM5CTL0		; disable DIO low-power default
 
 main:
-				bis.b	#BIT6, &P6OUT
-				call 	#delay_init
-				call 	#i2c_start
-				bic.b	#BIT6, &P6OUT
-				call 	#delay_init
-				jmp 	main
+					; start condition
+					bis.b	#BIT2, &P3OUT			; SCL
+					call 	#delay
+					call 	#i2c_start
+					bic.b	#BIT2, &P3OUT			; SCL
+					call 	#delay
+					; END start condition
+
+					; send
+					call	#i2c_send
+					; END send
+					jmp 	main
 
 
 
-delay_init:		mov.w	global_outer_delay, R4
-delay:			mov.w	global_inner_delay, R5	; sets inner loop delay
-dec_inner:		dec		R5						; decrements inner delay reg
-				jnz		dec_inner				; loop if R5 is not 0
-				dec		R4						; decrements outer delay reg
-				jnz		delay					; loop if R4 is not 0
-				ret
+;-------------------------------------------------------------------------------
+; Subroutines
+;-------------------------------------------------------------------------------
+
+short_delay:		mov.w	global_short_outer_delay, R4	; sets outer loop delay
+					mov.w	global_short_inner_delay, R6
+					jmp		inner_loop
+delay:				mov.w	global_outer_delay, R4	; sets outer loop delay
+					mov.w	global_inner_delay, R6
+					jmp 	inner_loop
+inner_loop:			mov.w	R6, R5					; sets inner loop delay
+					jmp		dec_inner
+dec_inner:			dec		R5						; decrements inner delay reg
+					jnz		dec_inner				; loop if R5 is not 0
+					dec		R4						; decrements outer delay reg
+					jnz		inner_loop				; loop if R4 is not 0
+					ret
+
+
 
 i2c_start:
-				bis.b	#BIT0, &P1OUT
-				call 	#delay_init
-				bic.b	#BIT0, &P1OUT
-				call 	#delay_init
-				ret
+					; start condition implies that SCL must be high and SDA must change from H->L then SCL->L
+					bis.b	#BIT3, &P3OUT			; SDA
+					call 	#delay
+					bic.b	#BIT3, &P3OUT			; SDA
+					call 	#delay
+					ret
+
+
+
+i2c_send:
+					call	#i2c_tx_byte
+					ret
+
+
+
+i2c_tx_byte:
+					mov.w	#06h, R7				; loop counter
+					mov.b	transmit_byte, R8		; i2c addr
+					rlc 	R8
+for:				dec		R7
+					; send bit
+					and.b	R8, #080h				; bit mask: 1000 0000 0000 0000b = 080000h, 1000 0000 = 080h
+					jz		sda_low					; if z=1 then MSB is 0 and dont set SDA
+					bis.b	#BIT3, &P3OUT			; SDA high
+					jmp		stab_delay				; skip to stability delay
+sda_low:			bis.b	#BIT3, &P3OUT			; SDA low
+stab_delay:			call	#short_delay			; stability delay
+					bis.b	#BIT2, &P3OUT			; SCL high
+					call 	#delay					; bit delay
+					bic.b	#BIT2, &P3OUT			; SCL low
+					call	#short_delay			; stability delay
+					bic.b	#BIT3, &P3OUT			; SDA low
+					; END send bit
+					rra		R8						; rotate i2c addr
+for_cmp:			cmp		#00h, R7				; compare R7 to 0
+					jnz		for						; if R7 is not 0 then continue iterating
+					; TODO send r/w
+					ret
 
 ;-------------------------------------------------------------------------------
 ; Interrupt Service Routines
@@ -87,31 +138,36 @@ i2c_start:
 
 ; Service TB0
 timer_b0_isr:
-			bic.w	#TBIFG, &TB0CTL
-			reti
+					bic.w	#TBIFG, &TB0CTL
+					reti
 ;-------------- END service_TB0 --------------
                                             
 ;-------------------------------------------------------------------------------
 ; Memory Allocation
 ;-------------------------------------------------------------------------------
 
-						.data
-						.retain
+					.data
+					.retain
 
-global_outer_delay:		.short	00BD3h
-global_inner_delay:		.short  00072h
+global_short_outer_delay:	.short	00BD3h
+global_short_inner_delay:	.short  00072h
+
+global_outer_delay:	.short	00BD3h
+global_inner_delay:	.short  00072h
+
+transmit_byte:		.byte  00068h					; DS3231 Addr: 1101000 = 068h
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
-            .global __STACK_END
-            .sect   .stack
+            		.global __STACK_END
+            		.sect   .stack
             
 ;-------------------------------------------------------------------------------
 ; Interrupt Vectors
 ;-------------------------------------------------------------------------------
-            .sect   ".reset"                ; MSP430 RESET Vector
-            .short  RESET
-            
-            .sect	".int43"				; TB0CCR0
-            .short	timer_b0_isr
+	            	.sect   ".reset"                ; MSP430 RESET Vector
+	            	.short  RESET
+
+	            	.sect	".int43"				; TB0CCR0
+	            	.short	timer_b0_isr
