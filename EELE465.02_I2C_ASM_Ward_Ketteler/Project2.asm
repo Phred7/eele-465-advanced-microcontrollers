12353ef7e34dc0bd7b8cba1ed8ab9fd6ef48ff26
wward <woward00@gmail.com>
2022-02-01 12:55:59 -0700

    Increased subroutine reuse in P2

--- EELE465.02_I2C_ASM_Ward_Ketteler/Project2.asm
+++ EELE465.02_I2C_ASM_Ward_Ketteler/Project2.asm
@@ -69,6 +69,19 @@ main:
 					rlc.b 	R8
 					call	#i2c_send
 
+					; send data (0-9)
+					mov.b 	#00h, R11
+					mov.b	#0Ah, R10				; loop counter (08h sends 8 bits)
+for:				dec.b	R10
+					mov.b	R11, R8
+					call	#i2c_send
+					add.b	#01h, R11				; add 1 to send data
+					cmp		#00h, R10				; compare R10 to 0
+					jnz		for						; if R10 is not 0 then continue iterating
+
+					; stop condition
+					call	#i2c_stop				; sends i2c stop condition (same as re-start condition but should only happen after an ACK. If a NACK resend)
+
 					jmp 	main
 
 
@@ -119,18 +132,7 @@ i2c_stop:
 
 i2c_send:
 					call	#i2c_tx_byte
-
-					; send data (0-9)
-					mov.b 	#00h, R11
-					mov.b	#0Ah, R10				; loop counter (08h sends 8 bits)
-for:				dec.b	R10
-					mov.b	R11, R8
-					call	#i2c_tx_byte
-					add.b	#01h, R11				; add 1 to send data
-					cmp		#00h, R10				; compare R10 to 0
-					jnz		for						; if R10 is not 0 then continue iterating
-
-					call	#i2c_stop				; sends i2c stop condition (same as re-start condition but should only happen after an ACK. If a NACK resend)
+					call	#ack					; simulates recieving an ACK
 					ret
 
 
@@ -152,7 +154,6 @@ stab_delay:			call	#clock_pulse
 					rla.b	R8						; rotate byte to next bit
 tx_byte_for_cmp:	cmp		#00h, R7				; compare R7 to 0
 					jnz		tx_byte_for				; if R7 is not 0 then continue iterating
-					call	#ack					; simulates recieving an ACK
 					ret
 
 
