9a1636437cafa572e2c7f75d582199e67cb31635
W. Ward <woward00@gmail.com>
2022-02-02 10:15:32 -0700

    Project for debugging Haley's code

---
+++ haley_debug/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2355.cmd 
+
+ASM_SRCS += \
+../EELE465.01_I2C_Ketteler_Ward_v1.asm 
+
+OBJS += \
+./EELE465.01_I2C_Ketteler_Ward_v1.obj 
+
+ASM_DEPS += \
+./EELE465.01_I2C_Ketteler_Ward_v1.d 
+
+OBJS__QUOTED += \
+"EELE465.01_I2C_Ketteler_Ward_v1.obj" 
+
+ASM_DEPS__QUOTED += \
+"EELE465.01_I2C_Ketteler_Ward_v1.d" 
+
+ASM_SRCS__QUOTED += \
+"../EELE465.01_I2C_Ketteler_Ward_v1.asm" 
+
+
