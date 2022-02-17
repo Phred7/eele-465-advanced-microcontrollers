2239b88269fd520bc2b9555cfc74ec87214f74ed
wward <woward00@gmail.com>
2022-02-17 13:50:32 -0700

    Project 3: A, B, C working

--- EELE465.03_Keypad_ASM_Ward_Ketteler/Project3.asm
+++ EELE465.03_Keypad_ASM_Ward_Ketteler/Project3.asm
@@ -10,6 +10,7 @@
 ; R6: Pattern B mask
 ; R7: Pattern C mask
 ; R8: Pattern D mask
+; R9: Timer Interrupt Keypad Compare [1h: 'A', 2h: 'B', 3h: 'C', 4h: 'D']
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
             
@@ -64,8 +65,10 @@ init:
 			mov.w	#00h, R4
 			mov.w	#00h, R5
 			mov.w	#00h, R6
-			mov.w	#00h, R7
+			mov.w	#01111111b, R7
 			mov.w	#00h, R8
+
+			bic.b	#0FFFFh, &P3OUT
 
 main:
 			; while
@@ -93,39 +96,43 @@ p_d:		call	#pattern_d
 p_end:
 			cmp.b	#00h, R4
 			jz		end_main				; if R4 is not 00h then move R4 into R6
-			mov.w 	R4, R6
+			mov.w 	R4, R5
 end_main:
 			mov.w	#00h, R4
 			jmp 	main
 			nop
 
 check_keypad:
-			mov.b	#021h, R4
+			mov.b	#041h, R4
+			mov.b	#01h, R5
 			ret
 
 pattern_a:
 			bic.w	#CCIE, &TB0CCTL0		; disable timer interrupt
-			bis.b	pattern_A_bit_mask, &P3OUT
+			mov.b	#01h, R9
+			mov.b	pattern_A_bit_mask, &P3OUT
 			ret
 
 pattern_b:
-			mov.w	#15625d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for 1Hz
+			mov.w	#1562d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for 1Hz
+			mov.b	#02h, R9
 			bis.w	#CCIE, &TB0CCTL0
 			cmp.b	R4, R5					; if the last button press was B and this button press was B z=1 and restart the pattern
 			jnz		b_cmp_end
 			mov.w	#00h, R6
-b_cmp_end:	mov.b	#008h, R6				; test code
-			bis.b	R6, &P3OUT
+b_cmp_end:
+			mov.b	R6, &P3OUT
 			ret
 
 pattern_c:
-			mov.w	#32992d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for 1Hz
+			mov.w	#3299d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for 1Hz
+			mov.b	#03h, R9
 			bis.w	#CCIE, &TB0CCTL0
-			mov.b	#0FFh, R7				; test code
-			bis.b 	R7, &P3OUT
+			mov.b 	R7, &P3OUT
 			ret
 
 pattern_d:
+			mov.b	#04h, R9
 			ret
 
 unlock_code:
@@ -138,9 +145,29 @@ unlock_code:
 
 ; Service TB0
 TimerB0_ISR:
-;			xor.b	#BIT6, &P6OUT
-			bic.w	#TBIFG, &TB0CTL
-			reti
+				cmp.b	#02h, R9	; if R9 is 02h then the current pattern is pattern B
+				jz		update_pattern_b
+				cmp.b	#03h, R9	; if R9 is 03h then the current pattern is pattern C
+				jz		update_pattern_c
+				cmp.b	#04h, R9	; if R9 is 04h then the current pattern is pattern D
+				jz		update_pattern_d
+end_tb0_isr:	bic.w	#TBIFG, &TB0CTL
+				reti
+
+update_pattern_b:
+				inc.b	R6
+				jmp 	end_tb0_isr
+update_pattern_c:
+				rrc.b	R7
+				cmp.b	#0FFh, R7		; if R7 is 0FFh, reset to starting value
+				jnz		end_tb0_isr
+				mov.w	#01111111b, R7
+				jmp 	end_tb0_isr
+update_pattern_d:
+				jmp 	end_tb0_isr
+				nop
+
+
 ;-------------- END service_TB0 --------------
 
 ;-------------------------------------------------------------------------------
