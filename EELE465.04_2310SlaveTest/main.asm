cb5eab1239fae462fa63a41ca0e2190e2e4aa716
W. Ward <woward00@gmail.com>
2022-02-28 21:52:10 -0700

    Add slave test for p4

--- EELE465.04_2310SlaveTest/main.asm
+++ EELE465.04_2310SlaveTest/main.asm
@@ -1,9 +1,6 @@
 ;-------------------------------------------------------------------------------
 ; MSP430 Assembler Code Template for use with TI Code Composer Studio
-; Walker Ward
-; EELE371
-; 1/20/2022
-; Project 1: LED Heartbeat
+;
 ;
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
@@ -27,62 +24,52 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 init:
-			bis.b	#BIT6, &P6DIR ; set green LED2 as out
-			bic.b	#BIT6, &P6OUT ; set init val to 0
+			bis.b	#BIT0, &P1DIR			; Set as output P1.0
+			bic.b	#BIT0, &P1OUT
 
-			bis.b	#BIT0, &P1DIR ; set red LED1 as out
-			bic.b	#BIT0, &P1OUT ; set init val to 0
+			bis.b	#BIT1, &P1DIR			; Set as output P1.1
+			bic.b	#BIT1, &P1OUT
 
-			; setup timber TB0: delta-5 = 1sec
-			bis.w	#TBCLR, &TB0CTL 		; clears timers and dividers
-			bis.w	#TBSSEL__SMCLK, &TB0CTL	; choose clock (f = 1 MHz)
-			bis.w	#MC__UP, &TB0CTL		; choose mode (UP)
+			bis.b	#BIT4, &P1DIR			; Set as output P1.4
+			bic.b	#BIT4, &P1OUT
 
-			bis.w	#CNTL_0, &TB0CTL		; choose counter length (N = 2^16)
-			bis.w	#ID__4, &TB0CTL			; choose divider for D1 (D1 = 4)
-			bis.w 	#TBIDEX__8, &TB0EX0		; choose divider for D2 (D2 = 8)
+			bis.b	#BIT5, &P1DIR			; Set as output P1.5
+			bic.b	#BIT5, &P1OUT
 
-			; TB0 interrupt: Compare
-			mov.w	#32992d, &TB0CCR0		; N = 15625: TB0 @ 0.5sec, N = 32992d for 1Hz
-			bis.w	#CCIE, &TB0CCTL0
-			bic.w	#CCIFG, &TB0CCTL0
+			bis.b	#BIT6, &P1DIR			; Set as output P1.6
+			bic.b	#BIT6, &P1OUT
 
-			nop
-			eint							; assert global interrupt flag
-			nop
+			bis.b	#BIT7, &P1DIR			; Set as output P1.7
+			bic.b	#BIT7, &P1OUT
+
+			bis.b	#BIT6, &P2DIR			; Set as output P2.6
+			bic.b	#BIT6, &P2OUT
+
+			bis.b	#BIT7, &P2DIR			; Set as output P2.7
+			bic.b	#BIT7, &P2OUT
 
 			bic.b	#LOCKLPM5, &PM5CTL0		; disable DIO low-power default
 
 main:
-			call #flash_red
-			jmp main
+			xor.b	#011110011b, &P1OUT
+			xor.b	#011000000b, &P2OUT
+			call	#delay
+			jmp 	main
 			nop
 
-flash_red:
-				xor.b	#BIT0, &P1OUT
-				mov.w	global_outer_delay, R4
-				call 	#delay
-				ret
+                                            
 
-delay:			mov.w	global_outer_delay, R4
+;-------------------------------------------------------------------------------
+; Subroutines
+;-------------------------------------------------------------------------------
+
+delay:			mov.w	global_outer_delay, R4  ; sets outer loop delay
 delay_gid:		mov.w	global_inner_delay, R5	; sets inner loop delay
 dec_inner:		dec		R5						; decrements inner delay reg
 				jnz		dec_inner				; loop if R5 is not 0
 				dec		R4						; decrements outer delay reg
 				jnz		delay_gid				; loop if R4 is not 0
 				ret
-
-
-;-------------------------------------------------------------------------------
-; Interrupt Service Routines
-;-------------------------------------------------------------------------------
-
-; Service TB0
-TimerB0_ISR:
-			xor.b	#BIT6, &P6OUT
-			bic.w	#TBIFG, &TB0CTL
-			reti
-;-------------- END service_TB0 --------------
 
 ;-------------------------------------------------------------------------------
 ; Memory Allocation
@@ -91,11 +78,11 @@ TimerB0_ISR:
 					.data
 					.retain
 
-;global_outer_delay:		.short	00BD3h
-;global_inner_delay:		.short  00072h
+global_outer_delay:		.short	00BD3h
+global_inner_delay:		.short  00072h
 
-global_outer_delay:		.short	00005h
-global_inner_delay:		.short  00002h
+;global_outer_delay:		.short	00005h
+;global_inner_delay:		.short  00002h
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
@@ -109,6 +96,3 @@ global_inner_delay:		.short  00002h
             .sect   ".reset"                ; MSP430 RESET Vector
             .short  RESET
             
-            .sect	".int43"				; TB0CCR0
-            .short	TimerB0_ISR
-
