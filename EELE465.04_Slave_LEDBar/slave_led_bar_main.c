2c181ddecf985f2c4375f0ac1ab83322111a72c6
W. Ward <woward00@gmail.com>
2022-03-08 01:59:45 -0700

    Secondary working!

--- EELE465.04_Slave_LEDBar/slave_led_bar_main.c
+++ EELE465.04_Slave_LEDBar/slave_led_bar_main.c
@@ -3,58 +3,75 @@
 char recievedData = 0x0;
 
 int main(void) {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
 
     PM5CTL0 &= ~LOCKLPM5;       // Enable GPIO
 
-	UCB0CTLW0 |= UCSWRST;
-
-	UCB0CTLW0 |= UCMODE_3 + UCSYNC;  // Choose I2C mode
-	UCB0CTLW0 &= ~UCMST;    // Choose to be slave
-	UCB0I2COA0 = 0x0012;    // slave addr
-	UCB0I2COA0 |= UCOAEN;  // use slave addr set above
-//	UCB0I2COA0 |= UCGCEN;   // Autorespond to general call
-	UCB0CTLW0 &= ~UCTR;     // set to receive
-	UCB0CTLW1 &= ~UCSWACK;  // auto ACK
-
-    UCB0CTLW0 &= ~UCSWRST;
-
-	P1SEL1 &= ~BIT3;         // P1.3 SCL
-	P1SEL0 |= BIT3;
-	P1SEL1 &= ~BIT2;        // P1.2 SDA
-	P1SEL0 |= BIT2;
-
-//	UCB0CTLW1 &= ~UCTXIFG0;
-
-
-
-	UCB0IE |= UCRXIE0;
-//	UCB0IE |= UCTXIE0;
-	__enable_interrupt();
-
-	P1DIR |= BIT0;        // P1.0
+    P1DIR |= BIT0;        // P1.0
     P1OUT &= ~BIT0;        // Init val = 0
-	P1DIR |= BIT1;        // P1.1
-    P1OUT &= ~BIT1;        // Init val = 0
 
-	P1OUT |= BIT0;
-	P1OUT |= BIT1;
+////    P1SEL1 &= ~BIT1;         // P1.1 SCL
+//    P1SEL0 |= BIT1;
+////    P1SEL1 &= ~BIT2;        // P1.2 SDA
+//    P1SEL0 |= BIT2;
 
-	int i;
-	while(1) {
-        for(i=0; i<100; i=i+1) {}
-	}
+    P4SEL1 &= ~BIT7;            // P4.7 = SCL
+    P4SEL0 |= BIT7;
 
-	return 0;
+    P4SEL1 &= ~BIT6;            // P4.6 = SDA
+    P4SEL0 |= BIT6;
+
+    UCB1CTLW0 |= UCSWRST;
+
+    UCB1CTLW0 |= UCSSEL_3;
+    UCB1BRW = 1;
+    UCB1CTLW0 |= UCMODE_3 + UCSYNC;           // I2C mode, sync mode (Do not set clock in slave mode)
+
+    UCB1CTLW0 &= ~UCMST;
+    UCB1I2COA0 = 0x12 | UCOAEN;               // Slave address is 0x77; enable it
+    UCB1CTLW0 &= ~UCTR;     // set to receive
+//    UCB1CTLW1 &= ~UCSWACK;  // auto ACK
+
+//  UCB0CTLW0 |= UCMODE_3 + UCSYNC;  // Choose I2C mode
+//  UCB0CTLW0 &= ~UCMST;    // Choose to be slave
+//  UCB0I2COA0 = 0x0012;    // slave addr
+//  UCB0I2COA0 |= UCOAEN;  // use slave addr set above
+////    UCB0I2COA0 |= UCGCEN;   // Autorespond to general call
+//  UCB0CTLW0 &= ~UCTR;     // set to receive
+//  UCB0CTLW1 &= ~UCSWACK;  // auto ACK
+
+    UCB1CTLW0 &= ~UCSWRST;
+
+//  P1SEL1 &= ~BIT3;         // P1.3 SCL
+//  P1SEL0 |= BIT3;
+//  P1SEL1 &= ~BIT2;        // P1.2 SDA
+//  P1SEL0 |= BIT2;
+//
+//  UCB0CTLW1 &= ~UCTXIFG0;
+//  UCB0CTLW1 &= ~UCRXIFG0;
+
+    UCB1IE |= UCRXIE0;
+    UCB1IE |= UCTXIE0;
+    __enable_interrupt();
+
+    P1OUT |= BIT0;
+
+    int i;
+    while(1) {
+        UCB1IFG &= ~UCSTPIFG;           // clear STOP flag
+    }
+
+    return 0;
 }
 
 //-- Service I2C
-#pragma vector = EUSCI_B0_VECTOR
-__interrupt void EUSCI_B0_I2C_ISR(void) {
-    UCB0IE &= ~UCRXIE0;
-//    UCB0IE &= ~UCTXIE0;
-    recievedData = UCB0RXBUF;
+#pragma vector = EUSCI_B1_VECTOR
+__interrupt void EUSCI_B1_I2C_ISR(void) {
+    UCB1IE &= ~UCRXIE0;
+    UCB1IE &= ~UCTXIE0;
+    recievedData = UCB1RXBUF;
+    UCTXACK_0;
     P1OUT &= ~BIT0;
     return;
 //    switch(UCB0IV){
