490efeb942e021568cacf971aaacf71c7f096255
W. Ward <woward00@gmail.com>
2022-03-18 22:08:53 -0600

    Update slave_led_bar_main.c

--- EELE465.04_Slave_LEDBar/slave_led_bar_main.c
+++ EELE465.04_Slave_LEDBar/slave_led_bar_main.c
@@ -1,22 +1,18 @@
 #include <msp430.h> 
 
-char recievedData = 0x0;
 
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+char recievedData = 0x00;
+int currentPattern = 0;
+int passcodeEnteredCorrectly = 0;
+unsigned int patternBMask = 0x00;
+unsigned int patternCMask = 0x07F;
+unsigned int patternDMask = 0x018;
 
 
-    PM5CTL0 &= ~LOCKLPM5;       // Enable GPIO
-
-    P1DIR |= BIT0;        // P1.0
-    P1OUT &= ~BIT0;        // Init val = 0
-
-    P1DIR |= BIT1;        // P1.1
-    P1OUT &= ~BIT1;        // Init val = 0
-
-    P1SEL1 &= ~BIT3;         // P1.3 SCL
+void configI2C(void) {
+    P1SEL1 &= ~BIT3;      // P1.3 SCL
     P1SEL0 |= BIT3;
-    P1SEL1 &= ~BIT2;        // P1.2 SDA
+    P1SEL1 &= ~BIT2;      // P1.2 SDA
     P1SEL0 |= BIT2;
 
     UCB0CTLW0 |= UCSWRST;
@@ -36,6 +32,40 @@ int main(void) {
 
     UCB0IE |= UCRXIE0;
     UCB0IE |= UCTXIE0;
+    return;
+}
+
+
+void configTimer(void){
+    // Timers
+    // TB0
+    TB0CTL |= TBCLR;                // Clear timer and divs
+    TB0CTL |= TBSSEL__SMCLK;        // SRC = SMCLK
+    TB0CTL |= MC__UP;               // Mode = UP
+    TB0CTL |= CNTL_0;               // Length = 16-bit
+    TB0CTL |= ID__8;                // ste d1 to 8
+    TB0EX0 |= TBIDEX__7;            // set d2 to 7
+    TB0CCR0 = 9366;                 // CCR0 = (1 s) w/ d2 = 7 1sec: 18732, .5sec = 9366 From pg 297 or TB
+    return;
+}
+
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+
+    PM5CTL0 &= ~LOCKLPM5;       // Enable GPIO
+
+    P1DIR |= BIT0;        // P1.0
+    P1OUT &= ~BIT0;       // Init val = 0
+
+    P1DIR |= BIT1;        // P1.1
+    P1OUT &= ~BIT1;       // Init val = 0
+
+    P3DIR |= 0x0FF;
+    P3OUT &= ~0x0FF;
+
+    configI2C();
 
     __enable_interrupt();
 
@@ -43,11 +73,30 @@ int main(void) {
     P1OUT |= BIT1;
 
     int i;
-    while(1);
+    while(1) {
+        if (passcodeEnteredCorrectly == 1) {
+            switch (currentPattern) {
+            case 0: // A
+                P3OUT &= 0x0AA;
+                break;
+            case 1: // B
+
+                break;
+            case 2: // C
+                break;
+            case 3: // D
+                break;
+            }
+        } else {
+            passcodeEnteredCorrectly = 1;
+        }
+    }
 
     return 0;
 }
 
+
+//-- Interrupt Service Routines -------------------------
 //-- Service I2C
 #pragma vector = EUSCI_B0_VECTOR
 __interrupt void EUSCI_B0_I2C_ISR(void) {
@@ -58,3 +107,19 @@ __interrupt void EUSCI_B0_I2C_ISR(void) {
 }
 //-- END EUSCI_B0_I2C_ISR
 
+//-- Service TB0
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void ISR_TB0_CCR0(void) {
+    switch (currentPattern) {
+    case 0:
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    }
+    TB0CCTL0 &= ~CCIFG;         // Clear CCR0 flag
+}
+//-- END TB0 ISR
