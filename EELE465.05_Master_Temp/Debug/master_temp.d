ca8fff4fed25502212f48e5d58c013f314825df4
W. Ward <woward00@gmail.com>
2022-03-29 01:50:13 -0600

    Add P5 main

---
+++ EELE465.05_Master_Temp/Debug/master_temp.d
@@ -0,0 +1,24 @@
+# FIXED
+
+master_temp.obj: ../master_temp.c
+master_temp.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h
+master_temp.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2355.h
+master_temp.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h
+master_temp.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+master_temp.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+master_temp.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h
+
+../master_temp.c:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h:
+
---
+++ EELE465.05_Master_Temp/master_temp.c
@@ -0,0 +1,130 @@
+#include <msp430.h> 
+
+/** W. Ward
+ *  03/29/2022
+ *  I2C Main
+ */
+
+int dataSent = 0;
+unsigned int dataToSendI2C = 0x00;
+
+void configI2C(void) {
+    //-- Config. I2C Master
+    //-- Put eUSCI_B0 into sw reset
+    UCB1CTLW0 |= UCSWRST;
+    UCB1CTLW0 |= UCSSEL_3;
+    UCB1BRW = 10;
+    UCB1CTLW0 |= UCMODE_3;
+    UCB1CTLW0 |= UCMST;
+    UCB1CTLW0 |= UCTR;
+    UCB1I2CSA = 0x0042;
+    UCB1CTLW1 |= UCASTP_2;      // Auto STOP when UCB0TBCNT reached
+    UCB1TBCNT = 2; // # of Bytes in Packet
+    //-- Config I2C Ports
+    P4SEL1 &= ~BIT7;            // P4.7 = SCL
+    P4SEL0 |= BIT7;
+    P4SEL1 &= ~BIT6;            // P4.6 = SDA
+    P4SEL0 |= BIT6;
+
+    UCB1CTLW0 &= ~UCSWRST;
+
+    UCB1IE |= UCTXIE0;
+    //-- END Config. I2C Master
+    return;
+}
+
+void configTimer(void){
+    // Timers
+    // TB0
+    TB0CTL |= TBCLR;                // Clear timer and divs
+    TB0CTL |= TBSSEL__SMCLK;        // SRC = SMCLK
+    TB0CTL |= MC__UP;               // Mode = UP
+    TB0CTL |= CNTL_0;               // Length = 16-bit
+    TB0CTL |= ID__8;                // ste d1 to 8
+    TB0EX0 |= TBIDEX__7;            // set d2 to 7
+    TB0CCR0 = 9366;                 // CCR0 = (1 s) w/ d2 = 7 1sec: 18732, .5sec = 9366 From pg 297 or TB
+    return;
+}
+
+void enableTimerInterrupt(int timerCompareValue){
+    // IRQs
+    // Timer Compare IRQ
+    TB0CCR0 = timerCompareValue;
+    TB0CCTL0 |= CCIE;               // Enable TB0 CCR0 overflow IRQ
+    TB0CCTL0 &= ~CCIFG;             // Clear CCR0 flag
+}
+
+
+void disableTimerInterrupt() {
+    TB0CCTL0 &= ~CCIE;              // Disable TB0 CCR0 overflow IRQ
+    TB0CCTL0 &= ~CCIFG;             // Clear CCR0 flag
+    return;
+}
+
+
+int delay(int delay){
+    int zzz;
+    for(zzz=0; zzz<delay; zzz++){}
+    return 0;
+}
+
+int send_i2c(unsigned int dataToSend) {
+    dataToSendI2C = dataToSend;
+
+    UCB1I2CSA = 0x0069;
+    UCB1CTLW0 |= UCTXSTT;
+    while (UCB0CTLW0 & UCTXSTP);
+
+    dataToSendI2C = 0x00;
+
+    return 0;
+}
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P1DIR |= BIT0;        // P1.0 LED 1
+    P1OUT &= ~BIT0;        // Init val = 0
+    P6DIR |= BIT6;        // P6.6 LED 2
+    P6OUT &= ~BIT6;        // Init val = 0
+
+    PM5CTL0 &= ~LOCKLPM5;       // disable LPM
+
+    configI2C();
+
+    configTimer();
+
+    __enable_interrupt();
+
+    while(1){};
+	return 0;
+}
+//-- END main
+
+
+//-- Interrupt Service Routines -------------------------
+//-- Service I2C B1
+#pragma vector=EUSCI_B1_VECTOR
+__interrupt void EUSCI_B1_I2C_ISR(void){
+    if (dataSent == 1) {
+        UCB1IFG &= ~UCTXIFG0;
+        UCB1CTL1 |= UCTXSTP;
+        dataSent = 0;
+    } else {
+        UCB1TXBUF = dataToSendI2C;
+        dataSent = 1;
+        UCB1IFG &= ~UCTXIFG0;
+    }
+    return;
+
+}
+//-- END I2C B1 ISR
+
+//-- Service TB0
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void ISR_TB0_CCR0(void) {
+    P1OUT ^= BIT0;
+    TB0CCTL0 &= ~CCIFG;         // Clear CCR0 flag
+}
+//-- END TB0 ISR
