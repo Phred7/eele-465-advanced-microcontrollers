78952d9b58013695520ac293d576ed6baf1cb7d4
W. Ward <woward00@gmail.com>
2022-04-16 23:29:08 -0600

    mods to master code

---
+++ EELE465.6_Slave_I2C_led/Debug/slave6_i2c_led.d
@@ -0,0 +1,24 @@
+# FIXED
+
+slave6_i2c_led.obj: ../slave6_i2c_led.c
+slave6_i2c_led.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h
+slave6_i2c_led.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2310.h
+slave6_i2c_led.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h
+slave6_i2c_led.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+slave6_i2c_led.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+slave6_i2c_led.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h
+
+../slave6_i2c_led.c:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2310.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h:
+
---
+++ EELE465.6_Slave_I2C_led/Debug/subdir_vars.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2310.cmd 
+
+C_SRCS += \
+../slave6_i2c_led.c 
+
+C_DEPS += \
+./slave6_i2c_led.d 
+
+OBJS += \
+./slave6_i2c_led.obj 
+
+OBJS__QUOTED += \
+"slave6_i2c_led.obj" 
+
+C_DEPS__QUOTED += \
+"slave6_i2c_led.d" 
+
+C_SRCS__QUOTED += \
+"../slave6_i2c_led.c" 
+
+
