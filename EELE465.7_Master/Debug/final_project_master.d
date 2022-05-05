2ce270f776fb2d9632ca38766f8422bfdbad829f
W. Ward <woward00@gmail.com>
2022-05-05 00:15:13 -0600

    init final project

---
+++ EELE465.7_Master/Debug/final_project_master.d
@@ -0,0 +1,24 @@
+# FIXED
+
+final_project_master.obj: ../final_project_master.c
+final_project_master.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h
+final_project_master.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2355.h
+final_project_master.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h
+final_project_master.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h
+final_project_master.obj: C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h
+final_project_master.obj: C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h
+
+../final_project_master.c:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics.h:
+
+C:/ti/ccs1040/ccs/tools/compiler/ti-cgt-msp430_20.2.5.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1040/ccs/ccs_base/msp430/include/legacy.h:
+
