9f928a37e215322421f8d0db50ae69940ba01a78
W. Ward <woward00@gmail.com>
2022-05-11 03:53:31 -0600

    Modify LCD indicators slightly

--- EELE465.7_Due/final_project_due_v1/final_project_due_v1.ino
+++ EELE465.7_Due/final_project_due_v1/final_project_due_v1.ino
@@ -34,28 +34,28 @@ void setup()
   lcd.backlight();  //open the backlight =
    
   lcd.setCursor ( 0, 0 );            // go to the top left corner
-  lcd.print("VT:"); // write this string on the top row //"    Hello,world!    "
+  lcd.print("VelT:"); // write this string on the top row //"    Hello,world!    "
   lcd.setCursor ( 15, 0 );
   lcd.print("0");
   lcd.setCursor ( 17, 0 );
   lcd.print("RPM");
   
   lcd.setCursor ( 0, 1 );            // go to the 2nd row
-  lcd.print("VA: "); // pad string with spaces for centering
+  lcd.print("VelA: "); // pad string with spaces for centering
   lcd.setCursor ( 15, 1 );
   lcd.print("0");
   lcd.setCursor ( 17, 1 );
   lcd.print("RPM");
   
   lcd.setCursor ( 0, 2 );            // go to the third row
-  lcd.print("RT: "); // pad with spaces for centering
+  lcd.print("RotT: "); // pad with spaces for centering
   lcd.setCursor ( 15, 2 );
   lcd.print("0");
   lcd.setCursor ( 17, 2 );
   lcd.print("deg");
   
   lcd.setCursor ( 0, 3 );            // go to the fourth row
-  lcd.print("RA: ");
+  lcd.print("RotA: ");
   lcd.setCursor ( 15, 3 );
   lcd.print("0");
   lcd.setCursor ( 17, 3 );
